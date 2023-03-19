226fef49a704176667d9cc03429b9c745a5cb23b
xuandai-py <buinguyenxuandai@gmail.com>
2023-03-19 21:31:02 +0700

    update

--- src/components/HashService.jsx
+++ src/components/HashService.jsx
@@ -1,17 +1,12 @@
 import axios from 'axios';
 
-
 const hashGeneratorService = (inputData, round) => {
-    return axios.post('http://localhost:3334/hash-input', { inputData, round })
-
+    return axios.post('https://hash-gen-service.onrender.com/hash-input', { inputData, round })
 };
 
 const checkMatchService = (inputData, hashedData) => {
-    return axios.post('http://localhost:3334/compare-input', { inputData, hashedData })
-
+    return axios.post('https://hash-gen-service.onrender.com/compare-input', { inputData, hashedData })
 };
-
-
 
 export {
     hashGeneratorService,
